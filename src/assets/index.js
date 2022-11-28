095e3f2ecd4549d3bbd726baab64de7fb7b2f803
rafidimas17 <rafidimas1706@gmail.com>
2022-11-28 21:42:30 +0700

    upload project

---
+++ src/assets/index.js
@@ -0,0 +1,22 @@
+import RegisterBg from "./image/regis.jpg";
+import LoginBg from "./image/login.jpg";
+import IconFacebook from "./icon/facebook.svg";
+import IconTwitter from "./icon/twitter.svg";
+import IconInstagram from "./icon/instagram.svg";
+import IconTelegram from "./icon/telegram.svg";
+import IconGithub from "./icon/github.svg";
+import IconCities from "./icon/Cities.svg";
+import IconHiker from "./icon/Hiker.svg";
+import IconMountain from "./icon/Mountain.svg";
+export {
+  RegisterBg,
+  LoginBg,
+  IconFacebook,
+  IconGithub,
+  IconInstagram,
+  IconTelegram,
+  IconTwitter,
+  IconCities,
+  IconHiker,
+  IconMountain,
+};
